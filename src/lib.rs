00e10db4ed0b8f005a8528150c3a15604889a9a3
mnaga <mennat.abuelnaga@gmail.com>
2022-03-25 21:16:54 +0200

    feat: refactor consumer, test: add sim tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::clone;
+
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::LookupMap;
 use near_sdk::serde::{Serialize, Deserialize};
@@ -36,84 +38,54 @@ const GAS_FOR_FT_TRANSFER_CALL: Gas = 25_000_000_000_000 + GAS_FOR_RESOLVE_TRANS
 #[ext_contract(fpo)]
 trait FPO {
     fn get_price(&self, pair: String, provider: AccountId) -> Option<U128>;
-    // fn get_price_call(
-    //     &self,
-    //     pair: String,
-    //     provider: AccountId,
-    //     receiver_id: AccountId,
-    // ) -> Promise ;
-    // fn get_prices_call(
-    //     &self,
-    //     pairs: Vec<String>,
-    //     providers: Vec<AccountId>,
-    //     receiver_id: AccountId,
-    // ) -> Promise;
-    // fn aggregate_avg_call(
-    //     &self,
-    //     pairs: Vec<String>,
-    //     providers: Vec<AccountId>,
-    //     min_last_update: WrappedTimestamp,
-    //     receiver_id: AccountId,
-    // ) -> Promise;
-    // fn aggregate_median_call(
-    //     &self,
-    //     pairs: Vec<String>,
-    //     providers: Vec<AccountId>,
-    //     min_last_update: WrappedTimestamp,
-    //     receiver_id: AccountId,
-    // ) -> Promise ;
-    // fn aggregate_collect_call(
-    //     &self,
-    //     pairs: Vec<String>,
-    //     providers: Vec<AccountId>,
-    //     min_last_update: WrappedTimestamp,
-    //     receiver_id: AccountId,
-    // ) -> Promise;
+    fn get_prices(&self, pairs: Vec<String>, providers: Vec<AccountId>) -> Vec<Option<U128>>;
+    fn aggregate_avg(
+        &self,
+        pairs: Vec<String>,
+        providers: Vec<AccountId>,
+        min_last_update: WrappedTimestamp,
+    ) -> Option<U128>;
+    fn aggregate_median(
+        &self,
+        pairs: Vec<String>,
+        providers: Vec<AccountId>,
+        min_last_update: WrappedTimestamp,
+    ) -> Option<U128>;
 }
 
 #[ext_contract(ext_self)]
 trait RequestResolver {
-    // fn set_entry(&self, pair: String, provider: AccountId) -> Promise;
-    // fn set_collection(&mut self, pairs: Vec<String>, providers: Vec<AccountId>) -> PriceEntry;
-    // fn on_price_received(
-    //     &self,
-    //     sender_id: AccountId,
-    //     pairs: Vec<String>,
-    //     providers: Vec<AccountId>,
-    //     price_type: PriceType,
-    //     results: Vec<Option<U128>>,
-    // );
     fn get_price_callback(&self) -> Option<U128> ;
+    fn get_prices_callback(&self) -> Vec<Option<U128>> ;
+    fn aggregate_avg_callback(&self) -> Option<U128>;
+    fn aggregate_median_callback(&self) -> Option<U128>;
+
 }
 
-// #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize)]
-// pub struct PriceEntry {
-//     price: U128,                   // Last reported price
-//     decimals: u16,                 // Amount of decimals (e.g. if 2, 100 = 1.00)
-//     last_update: WrappedTimestamp, // Time or report
-// }
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize)]
+#[derive(Clone)]
+pub struct PriceEntry {
+    price: U128,             
+    sender: AccountId, 
+    price_type: PriceType,
+}
 
-// #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize)]
-// pub struct Outcome {
-//     entry: Option<Vec<PriceEntry>>,
-//     refund: Balance
-// }
 
-// #[derive(BorshDeserialize, BorshSerialize)]
-// pub struct Provider {
-//     pub pairs: LookupMap<String, PriceEntry>, // Maps "{TICKER_1}/{TICKER_2}" => PriceEntry - e.g.: ETHUSD => PriceEntry
-// }
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct Provider {
+    pub pairs: LookupMap<String, PriceEntry>, // Maps "{TICKER_1}/{TICKER_2}" => PriceEntry - e.g.: ETH/USD => PriceEntry
+}
 
-// impl Provider {
-//     pub fn new() -> Self {
-//         Self {
-//             pairs: LookupMap::new("ps".as_bytes()),
-//         }
-//     }
-//     pub fn set_pair(&mut self, pair: &String, entry: &PriceEntry) {
-//         self.pairs.insert(pair, entry);
-//     }
-// }
+impl Provider {
+    pub fn new() -> Self {
+        Self {
+            pairs: LookupMap::new("ps".as_bytes()),
+        }
+    }
+    pub fn set_pair(&mut self, pair: &String, price: &PriceEntry) {
+        self.pairs.insert(pair, price);
+    }
+}
 
 
 
@@ -123,17 +95,19 @@ trait RequestResolver {
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Requester {
     oracle: AccountId,
+    providers: LookupMap<AccountId, Provider>, // maps:  AccountId => Provider
 
 }
 
-// #[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, Debug)]
-// pub enum PriceType {
-//     Single,
-//     Multiple,
-//     Mean,
-//     Median,
-//     Collect, // same as multiple but with min_last_update
-// }
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, Debug)]
+#[derive(Clone, Copy)]
+pub enum PriceType {
+    Single,
+    Multiple,
+    Mean,
+    Median,
+    Collect, // same as multiple but with min_last_update
+}
 
 #[near_bindgen]
 impl Requester {
@@ -141,8 +115,48 @@ impl Requester {
     pub fn new(oracle: AccountId) -> Self {
         Self {
             oracle,
+            providers: LookupMap::new("p".as_bytes()),
+
         }
     }
+
+
+    pub fn on_price_received(
+        &mut self,
+        sender_id: AccountId,
+        pairs: Vec<String>,
+        providers: Vec<AccountId>,
+        price_type: PriceType,
+        results: Vec<Option<U128>>,
+    ){
+        log!("HELLO FROM REQUESTER on_price_received");
+        for provider in providers.iter(){
+            let provider_account_id = provider.clone();
+            let mut provider = self.providers.get(&provider).unwrap_or(Provider::new());
+            for (index, pair) in pairs.iter().enumerate(){
+                match results[index]{
+                    Some(result) => {
+                        let entry: PriceEntry = PriceEntry { price: result, sender: sender_id.clone(), price_type: price_type.clone() };
+
+                        provider.set_pair(&pair, &entry.clone());
+                    
+                    },
+                    None => log!("Not found"),
+                }
+            }
+            self.providers.insert(&provider_account_id, &provider);
+
+        }
+        
+    }
+    pub fn get_pair(&self, provider: AccountId, pair: String) -> PriceEntry{
+        let prov = self.providers.get(&provider).expect("no provider with this account id");
+        prov.pairs.get(&pair).expect("No pair found")
+    }
+    // pub fn get_pairs(&self, provider: AccountId) -> LookupMap<std::string::String, PriceEntry>{
+    //     let prov = self.providers.get(&provider).expect("no provider with this account id");
+    //     prov.pairs
+    // }
 
     pub fn get_price(
         &self,
@@ -183,6 +197,139 @@ impl Requester {
         log!("RETURNING PRICE{:?}", price);
         price
     }
+
+
+
+
+    pub fn get_prices(
+        &self,
+        pairs: Vec<String>, 
+        providers: Vec<AccountId>
+    ) -> Promise {
+        log!("REQUESTER GET_PRICE");
+        fpo::get_prices(
+            pairs.clone(), 
+            providers.clone(),
+            &self.oracle, 
+             NO_DEPOSIT, 
+            GAS_FOR_RESOLVE_TRANSFER
+        ).then(
+
+            ext_self::get_prices_callback(
+                &env::current_account_id(),
+                0, // yocto NEAR to attach to the callback
+                5_000_000_000_000 // gas to attach to the callback
+            )
+        )
+            
+    }
+    #[private]
+    pub fn get_prices_callback(&self) -> Vec<Option<U128>> {
+        if env::promise_results_count() != 1 {
+            log!("Expected a result on the callback");
+            return vec![None];
+        }
+        log!("get_price_callback+++++++");
+
+        // Get response, return false if failed
+        let prices: Vec<Option<U128>> = match env::promise_result(0) {
+            PromiseResult::Successful(value) => near_sdk::serde_json::from_slice::<Vec<Option<U128>>>(&value).unwrap(),
+            _ => { log!("Getting info from Pool Party failed"); return vec![None]; },
+        };
+
+        log!("RETURNING PRICE{:?}", prices);
+        prices
+    }
+
+    
+    pub fn aggregate_avg(
+        &self,
+        pairs: Vec<String>, 
+        providers: Vec<AccountId>,
+        min_last_update: WrappedTimestamp
+    ) -> Promise {
+        log!("REQUESTER GET_PRICE");
+        fpo::aggregate_avg(
+            pairs.clone(), 
+            providers.clone(),
+            min_last_update.clone(),
+            &self.oracle, 
+             NO_DEPOSIT, 
+            GAS_FOR_RESOLVE_TRANSFER
+        ).then(
+
+            ext_self::aggregate_avg_callback(
+                &env::current_account_id(),
+                0, // yocto NEAR to attach to the callback
+                5_000_000_000_000 // gas to attach to the callback
+            )
+        )
+            
+    }
+    #[private]
+    pub fn aggregate_avg_callback(&self) -> Option<U128> {
+        if env::promise_results_count() != 1 {
+            log!("Expected a result on the callback");
+            return None;
+        }
+        log!("get_price_callback+++++++");
+
+        // Get response, return false if failed
+        let avg: Option<U128> = match env::promise_result(0) {
+            PromiseResult::Successful(value) => near_sdk::serde_json::from_slice::<Option<U128>>(&value).unwrap(),
+            _ => { log!("Getting info from Pool Party failed"); return None; },
+        };
+
+        log!("RETURNING PRICE{:?}", avg);
+        avg
+    }
+
+
+    pub fn aggregate_median(
+        &self,
+        pairs: Vec<String>, 
+        providers: Vec<AccountId>,
+        min_last_update: WrappedTimestamp
+    ) -> Promise {
+        log!("REQUESTER GET_PRICE");
+        fpo::aggregate_median(
+            pairs.clone(), 
+            providers.clone(),
+            min_last_update.clone(),
+            &self.oracle, 
+             NO_DEPOSIT, 
+            GAS_FOR_RESOLVE_TRANSFER
+        ).then(
+
+            ext_self::aggregate_median_callback(
+                &env::current_account_id(),
+                0, // yocto NEAR to attach to the callback
+                5_000_000_000_000 // gas to attach to the callback
+            )
+        )
+            
+    }
+    #[private]
+    pub fn aggregate_median_callback(&self) -> Option<U128> {
+        if env::promise_results_count() != 1 {
+            log!("Expected a result on the callback");
+            return None;
+        }
+        log!("get_price_callback+++++++");
+
+        // Get response, return false if failed
+        let avg: Option<U128> = match env::promise_result(0) {
+            PromiseResult::Successful(value) => near_sdk::serde_json::from_slice::<Option<U128>>(&value).unwrap(),
+            _ => { log!("Getting info from Pool Party failed"); return None; },
+        };
+
+        log!("RETURNING PRICE{:?}", avg);
+        avg
+    }
+
+
+
+
 
 }
 
--- tests/sim/main.rs
+++ tests/sim/main.rs
@@ -1,8 +1,9 @@
 use near_fpo::FPOContractContract;
-use requester::RequesterContract;
+use requester::{RequesterContract, Requester};
 use near_sdk::json_types::U128;
 pub use near_sdk::json_types::{Base64VecU8, ValidAccountId, WrappedDuration, U64};
 use near_sdk_sim::{call, deploy, init_simulator, to_yocto, ContractAccount, UserAccount, transaction::ExecutionStatus};
+use serde_json::json;
 
 // use near_primitives::{views::FinalExecutionStatus, transaction::ExecutionStatus};
 
@@ -40,20 +41,20 @@ fn init() -> (UserAccount, ContractAccount<FPOContractContract>, ContractAccount
 fn simulate_get_price() {
     let (root, fpo, requester) = init();
 
-    let user1 = root.create_user("user1".to_string(), to_yocto("1000000"));
-    let user2 = root.create_user("user2".to_string(), to_yocto("1000000"));
-    call!(user1, fpo.new()).assert_success();
+    let provider1 = root.create_user("provider1".to_string(), to_yocto("1000000"));
+    let provider2 = root.create_user("provider2".to_string(), to_yocto("1000000"));
+    call!(provider1, fpo.new()).assert_success();
 
     // create a price pair, check if it exists, and get the value
-    call!(user1, fpo.create_pair("ETH/USD".to_string(), 8, U128(2000))).assert_success();
+    call!(provider1, fpo.create_pair("ETH/USD".to_string(), 8, U128(2000))).assert_success();
     call!(
-        user1,
-        fpo.pair_exists("ETH/USD".to_string(), user1.account_id())
+        provider1,
+        fpo.pair_exists("ETH/USD".to_string(), provider1.account_id())
     )
     .assert_success();
     let price_entry = call!(
-        user1,
-        fpo.get_entry("ETH/USD".to_string(), user1.account_id())
+        provider1,
+        fpo.get_entry("ETH/USD".to_string(), provider1.account_id())
     );
 
     // output and check the data
@@ -71,10 +72,10 @@ fn simulate_get_price() {
     // println!("requester.account_id() {:?}", requester.account_id());
 
 
-    call!(user2, requester.new(fpo.account_id())).assert_success();
+    call!(provider2, requester.new(fpo.account_id())).assert_success();
 
-    // call!(user2, requester.get_price("ETH/USD".to_string(), user1.account_id())).assert_success();
-    let outcome = call!(user2, requester.get_price("ETH/USD".to_string(), user1.account_id()));
+    // call!(provider2, requester.get_price("ETH/USD".to_string(), provider1.account_id())).assert_success();
+    let outcome = call!(provider2, requester.get_price("ETH/USD".to_string(), provider1.account_id()));
     // println!("{:?}", outcome.promise_results());
     match &outcome.promise_results()[2]{
         Some(res) => { 
@@ -85,6 +86,279 @@ fn simulate_get_price() {
         None => println!("Retrieved Nothing"),
     }
     
+
+}
+
+
+#[test]
+fn simulate_get_prices() {
+    let (root, fpo, requester) = init();
+
+    let provider1 = root.create_user("provider1".to_string(), to_yocto("1000000"));
+    let provider2 = root.create_user("provider2".to_string(), to_yocto("1000000"));
+    let user = root.create_user("user".to_string(), to_yocto("1000000"));
+
+
+    call!(root, fpo.new()).assert_success();
+    call!(root, requester.new(fpo.account_id())).assert_success();
+
+
+
+    // create a price pair, check if it exists, and get the value
+    call!(provider1, fpo.create_pair("ETH/USD".to_string(), 8, U128(2000))).assert_success();
+    call!(
+        provider1,
+        fpo.pair_exists("ETH/USD".to_string(), provider1.account_id())
+    )
+    .assert_success();
+    let price_entry = call!(
+        provider1,
+        fpo.get_entry("ETH/USD".to_string(), provider1.account_id())
+    );
+
+    // output and check the data
+    // println!(
+    //     "Returned Price: {:?}",
+    //     &price_entry.unwrap_json_value()
+    // );
+    debug_assert_eq!(
+        &price_entry.unwrap_json_value()["price"].to_owned(),
+        &"2000".to_string()
+    );
+
+
+    call!(provider2, fpo.create_pair("ETH/USD".to_string(), 8, U128(4000))).assert_success();
+    call!(
+        provider2,
+        fpo.pair_exists("ETH/USD".to_string(), provider2.account_id())
+    )
+    .assert_success();
+    let price_entry = call!(
+        provider2,
+        fpo.get_entry("ETH/USD".to_string(), provider2.account_id())
+    );
+    debug_assert_eq!(
+        &price_entry.unwrap_json_value()["price"].to_owned(),
+        &"4000".to_string()
+    );
+
+
+
+    // println!("fpo.account_id() {:?}", fpo.account_id());
+    // println!("requester.account_id() {:?}", requester.account_id());
+
+
+
+    // call!(provider2, requester.get_price("ETH/USD".to_string(), provider1.account_id())).assert_success();
+    let outcome = call!(provider2, requester.get_prices(vec!["ETH/USD".to_string(), "ETH/USD".to_string()], vec![provider1.account_id(), provider2.account_id()]));
+    println!("{:?}", outcome.promise_results());
+    match &outcome.promise_results()[2]{
+        Some(res) => { 
+            println!("Retrieved Value: {:?}", res.unwrap_json_value());
+            assert_eq!(res.unwrap_json_value(), json!(vec!["2000", "4000"]));
+            // assert_eq!(near_sdk::serde_json::to_vec(&res.unwrap_json_value()), near_sdk::serde_json::to_vec(vec!["2000","4000"]));
+        },
+        None => println!("Retrieved Nothing"),
+    }
+    
+
+}
+
+
+#[test]
+fn simulate_agg_avg() {
+    let (root, fpo, requester) = init();
+
+    let provider1 = root.create_user("provider1".to_string(), to_yocto("1000000"));
+    let provider2 = root.create_user("provider2".to_string(), to_yocto("1000000"));
+    let user = root.create_user("user".to_string(), to_yocto("1000000"));
+
+
+    call!(root, fpo.new()).assert_success();
+    call!(root, requester.new(fpo.account_id())).assert_success();
+
+
+
+    // create a price pair, check if it exists, and get the value
+    call!(provider1, fpo.create_pair("ETH/USD".to_string(), 8, U128(2000))).assert_success();
+    call!(
+        provider1,
+        fpo.pair_exists("ETH/USD".to_string(), provider1.account_id())
+    )
+    .assert_success();
+    let price_entry = call!(
+        provider1,
+        fpo.get_entry("ETH/USD".to_string(), provider1.account_id())
+    );
+
+    // output and check the data
+    // println!(
+    //     "Returned Price: {:?}",
+    //     &price_entry.unwrap_json_value()
+    // );
+    debug_assert_eq!(
+        &price_entry.unwrap_json_value()["price"].to_owned(),
+        &"2000".to_string()
+    );
+
+
+    call!(provider2, fpo.create_pair("ETH/USD".to_string(), 8, U128(4000))).assert_success();
+    call!(
+        provider2,
+        fpo.pair_exists("ETH/USD".to_string(), provider2.account_id())
+    )
+    .assert_success();
+    let price_entry = call!(
+        provider2,
+        fpo.get_entry("ETH/USD".to_string(), provider2.account_id())
+    );
+    debug_assert_eq!(
+        &price_entry.unwrap_json_value()["price"].to_owned(),
+        &"4000".to_string()
+    );
+
+
+
+    // println!("fpo.account_id() {:?}", fpo.account_id());
+    // println!("requester.account_id() {:?}", requester.account_id());
+
+
+
+    // call!(provider2, requester.get_price("ETH/USD".to_string(), provider1.account_id())).assert_success();
+    let outcome = call!(provider2, requester.aggregate_avg(vec!["ETH/USD".to_string(), "ETH/USD".to_string()], vec![provider1.account_id(), provider2.account_id()], U64(0)));
+    println!("{:?}", outcome.promise_results());
+    match &outcome.promise_results()[2]{
+        Some(res) => { 
+            println!("Retrieved Value: {:?}", res.unwrap_json_value());
+            assert_eq!(res.unwrap_json_value(), "3000");
+            // assert_eq!(near_sdk::serde_json::to_vec(&res.unwrap_json_value()), near_sdk::serde_json::to_vec(vec!["2000","4000"]));
+        },
+        None => println!("Retrieved Nothing"),
+    }
+    
+
+}
+
+
+#[test]
+fn simulate_agg_median() {
+    let (root, fpo, requester) = init();
+
+    let provider1 = root.create_user("provider1".to_string(), to_yocto("1000000"));
+    let provider2 = root.create_user("provider2".to_string(), to_yocto("1000000"));
+    let user = root.create_user("user".to_string(), to_yocto("1000000"));
+
+
+    call!(root, fpo.new()).assert_success();
+    call!(root, requester.new(fpo.account_id())).assert_success();
+
+
+    // create a price pair, check if it exists, and get the value
+    // call!(provider1, fpo.create_pair("ETH/USD".to_string(), 8, U128(2000))).assert_success();
+    // call!(
+    //     provider1,
+    //     fpo.pair_exists("ETH/USD".to_string(), provider1.account_id())
+    // )
+    // .assert_success();
+    // let price_entry = call!(
+    //     provider1,
+    //     fpo.get_entry("ETH/USD".to_string(), provider1.account_id())
+    // );
+
+    // // output and check the data
+    // // println!(
+    // //     "Returned Price: {:?}",
+    // //     &price_entry.unwrap_json_value()
+    // // );
+    // debug_assert_eq!(
+    //     &price_entry.unwrap_json_value()["price"].to_owned(),
+    //     &"2000".to_string()
+    // );
+
+
+    // call!(provider2, fpo.create_pair("ETH/USD".to_string(), 8, U128(4000))).assert_success();
+    // call!(
+    //     provider2,
+    //     fpo.pair_exists("ETH/USD".to_string(), provider2.account_id())
+    // )
+    // .assert_success();
+    // let price_entry = call!(
+    //     provider2,
+    //     fpo.get_entry("ETH/USD".to_string(), provider2.account_id())
+    // );
+    // debug_assert_eq!(
+    //     &price_entry.unwrap_json_value()["price"].to_owned(),
+    //     &"4000".to_string()
+    // );
+
+
+
+    // // println!("fpo.account_id() {:?}", fpo.account_id());
+    // // println!("requester.account_id() {:?}", requester.account_id());
+
+
+
+    // // call!(provider2, requester.get_price("ETH/USD".to_string(), provider1.account_id())).assert_success();
+    // let outcome = call!(provider2, requester.aggregate_median(vec!["ETH/USD".to_string(), "ETH/USD".to_string()], vec![provider1.account_id(), provider2.account_id()], U64(0)));
+    // println!("{:?}", outcome.promise_results());
+    // match &outcome.promise_results()[2]{
+    //     Some(res) => { 
+    //         println!("Retrieved Value: {:?}", res.unwrap_json_value());
+    //         assert_eq!(res.unwrap_json_value(), "3000");
+    //         // assert_eq!(near_sdk::serde_json::to_vec(&res.unwrap_json_value()), near_sdk::serde_json::to_vec(vec!["2000","4000"]));
+    //     },
+    //     None => println!("Retrieved Nothing"),
+    // }
+    
+
+}
+
+#[test]
+fn simulate_get_price_call() {
+    let (root, fpo, requester) = init();
+
+    let provider1 = root.create_user("provider1".to_string(), to_yocto("1000000"));
+    let user = root.create_user("user".to_string(), to_yocto("1000000"));
+
+
+    call!(root, fpo.new()).assert_success();
+    call!(root, requester.new(fpo.account_id())).assert_success();
+
+
+    // create a price pair, check if it exists, and get the value
+    call!(provider1, fpo.create_pair("ETH/USD".to_string(), 8, U128(2000))).assert_success();
+    call!(
+        provider1,
+        fpo.pair_exists("ETH/USD".to_string(), provider1.account_id())
+    )
+    .assert_success();
+    let price_entry = call!(
+        provider1,
+        fpo.get_entry("ETH/USD".to_string(), provider1.account_id())
+    );
+
+    debug_assert_eq!(
+        &price_entry.unwrap_json_value()["price"].to_owned(),
+        &"2000".to_string()
+    );
+
+    let outcome = call!(user, fpo.get_price_call("ETH/USD".to_string(), provider1.account_id(), requester.account_id()));
+
+    println!("{:?}", outcome.promise_results());
+
+    let fetched_entry = call!(user, requester.get_pair(provider1.account_id(), "ETH/USD".to_string()));
+    println!("res: {:?}", fetched_entry);
+    
+    match &fetched_entry.promise_results()[1]{
+        Some(res) => { 
+            println!("Retrieved Value: {:?}", res.unwrap_json_value());
+            assert_eq!(res.unwrap_json_value()["price"], "2000");
+            // assert_eq!(near_sdk::serde_json::to_vec(&res.unwrap_json_value()), near_sdk::serde_json::to_vec(vec!["2000","4000"]));
+        },
+        None => println!("Retrieved Nothing"),
+    }
+    
+
 
 }
 
